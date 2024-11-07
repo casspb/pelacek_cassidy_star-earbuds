1d004a363afcb5bf5bc998de5d3932fa79ab83a8
casspb <cassidypelacek@gmail.com>
2024-11-07 12:30:35 -0500

    adjusted links and styling
    adjusted the script linking and adjusted the styling of hotspots, as well as added images to the hotspots

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
 (() => {
-    console.log("IIFE Fired");
+  console.log("IIFE Fired");
+
   
-    
-    //Variables
-    const hotspots = document.querySelectorAll('.Hotspot');
+  //Variables
+  const hotspots = document.querySelectorAll('.Hotspot');
 
 
-  //functions
-  function showInfo(e) {
-    //console.log("showInfo called");
-    //console.log(e.currentTarget.slot);
-    let selected = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`);
-    //button and slot value = whatever the user is hovered over
-    //`$ allows for dynamic values
-    //console.log(selected)
-    gsap.to(selected, 1, {autoAlpha: 1});
-  }
-  
-  function hideInfo(e) {
-    //console.log("hideInfo called");
-    let selected = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`);
-    gsap.to(selected, 1, {autoAlpha: 0});
-  }
-  
-  //eventListeners
-  hotspots.forEach(hotspot => {
-    hotspot.addEventListener("mouseover", showInfo);
-    hotspot.addEventListener("mouseout", hideInfo);
-  })
-  
-  
-  
-  })();
+//functions
+function showInfo(e) {
+  //console.log("showInfo called");
+  //console.log(e.currentTarget.slot);
+  let selected = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`);
+  //button and slot value = whatever the user is hovered over
+  //`$ allows for dynamic values
+  //console.log(selected)
+  gsap.to(selected, 1, {autoAlpha: 1});
+}
+
+function hideInfo(e) {
+  //console.log("hideInfo called");
+  let selected = document.querySelector(`button[slot="${e.currentTarget.slot}"] > div`);
+  gsap.to(selected, 1, {autoAlpha: 0});
+}
+
+//eventListeners
+hotspots.forEach(hotspot => {
+  hotspot.addEventListener("mouseover", showInfo);
+  hotspot.addEventListener("mouseout", hideInfo);
+})
+
+
+
+})();
